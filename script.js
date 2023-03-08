7008b6264afa7f68b9b4ecc268cd42dcd0ab6439
Stefan <105825004+sstoj1@users.noreply.github.com>
2023-03-07 23:29:45 -0500

    Add computer choice, round winner, and win counter functions

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
 
-function winner(rock, paper, scissors) {
-    let winner = "";
-    if (rock == true && scissors == true) {
-        winner = "Rock";
-    } else if (rock == true && paper == true) {
-        winner = "Paper";
-    } else if (paper == true && scissors == true) {
-        winner = "Scissors"
-    } else {
-        winner = "Tie";
-    }
-
-    return winner;
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
+    return computerChoice;
 }
+console.log(getComputerChoice());
 
-console.log(winner(true, false, true)); // should be rock
-console.log(winner(true, true, false)); // should be paper
-console.log(winner(false, true, true)); // should be scissors
-
+function getRoundWinner(playerChoice)
